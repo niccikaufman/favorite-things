f7287e0c1400957bb9c10c2861f01eb574eb7cbd
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-05 14:46:15 -0700

    add form output section that pulls elements from the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-  var myArray = [];
+  let myArray = [];
   $('#submit').on('click', function() {
-    event.preventDefault()
+    event.preventDefault();
     $('.req').each(function() {
       myArray.push($(this).val()); 
-      console.log(myArray);
     });
-  });     
+    $("#fave-color-output").text(myArray[0]);
+    $("#fave-animal-output").text(myArray[1]);
+    $("#fave-season-output").text(myArray[2]);
+    $("#fave-band-output").text(myArray[3]);
+    $("#fave-movie-output").text(myArray[4]);
+    $('.output').show(); 
+  }); 
 });
